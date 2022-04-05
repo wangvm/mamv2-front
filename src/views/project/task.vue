2307cae488f027a75cf9e9f0e85aa24161337434
VM <465132131@qq.com>
2022-04-05 23:29:53 +0800

    🐛 FIX: 添加任务插叙接口fix

--- src/views/project/task.vue
+++ src/views/project/task.vue
@@ -1,33 +1,47 @@
 <template>
   <div>
-    <BaseHeader >
-    <template v-slot:left>
-        <el-button type="primary" @click="back" >返回</el-button>
-    </template>
+    <BaseHeader>
+      <template v-slot:left>
+        <el-button type="primary" @click="back">返回</el-button>
+      </template>
       <template v-slot:right>
-        <el-button type="primary" @click="addTask" >新建任务</el-button>
+        <el-button type="primary" @click="addTask">新建任务</el-button>
       </template>
     </BaseHeader>
     <el-main>
-      <el-table :data="tableData" stripe :style="{width:100+'%'}">
-          <el-table-column prop="index" label="序号"></el-table-column>
-          <el-table-column prop="name" label="任务名称">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.name" v-show="scope.row.isEdit" autofocus=true size='mini'></el-input>
-              <span v-show="!scope.row.isEdit">{{scope.row.name}}</span>
-            </template>
-          </el-table-column>
-          <el-table-column prop="catalogerName" label="编目员"></el-table-column>
-          <el-table-column prop="auditorName" label="审核员"></el-table-column>
-          <el-table-column prop="createTime" label="创建时间"></el-table-column>
-          <el-table-column prop="status" label="项目状态"></el-table-column>
-          <el-table-column label="" min-width="100%">
-            <template slot-scope="scope">
-              <el-button size="mini" @click="handleEdit(scope.row)">{{scope.row.isEdit?'保存':'编辑'}}</el-button>
-              <el-button size="mini" type="danger" @click="handleDelete(scope.$index,scope.row)">删除</el-button>
-              <el-button size="mini" type="primary" @click="enterTask(scope.row)">进入任务</el-button>
-            </template>
-          </el-table-column>
+      <el-table :data="tableData" stripe :style="{ width: 100 + '%' }">
+        <el-table-column prop="index" label="序号"></el-table-column>
+        <el-table-column prop="name" label="任务名称">
+          <template slot-scope="scope">
+            <el-input
+              v-model="scope.row.name"
+              v-show="scope.row.isEdit"
+              autofocus="true"
+              size="mini"
+            ></el-input>
+            <span v-show="!scope.row.isEdit">{{ scope.row.name }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="catalogerName" label="编目员"></el-table-column>
+        <el-table-column prop="auditorName" label="审核员"></el-table-column>
+        <el-table-column prop="createTime" label="创建时间"></el-table-column>
+        <el-table-column prop="status" label="项目状态"></el-table-column>
+        <el-table-column label="" min-width="100%">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.row)">{{
+              scope.row.isEdit ? "保存" : "编辑"
+            }}</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+              >删除</el-button
+            >
+            <el-button size="mini" type="primary" @click="enterTask(scope.row)"
+              >进入任务</el-button
+            >
+          </template>
+        </el-table-column>
       </el-table>
     </el-main>
     <el-footer class="pagination">
@@ -71,13 +85,18 @@
           ></el-autocomplete>
         </el-form-item>
         <el-form-item label="创建时间" prop="createTime">
-          <el-input :value="new Date(taskForm.createTime).toLocaleString()" disabled></el-input>
+          <el-input
+            :value="new Date(taskForm.createTime).toLocaleString()"
+            disabled
+          ></el-input>
         </el-form-item>
         <el-form-item label="项目状态" prop="status">
           <el-input :value="taskForm.status" disabled></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('taskForm')">提交</el-button>
+          <el-button type="primary" @click="submitForm('taskForm')"
+            >提交</el-button
+          >
           <el-button @click="resetForm('taskForm')">重置</el-button>
         </el-form-item>
       </el-form>
@@ -89,12 +108,12 @@
 import $api from "@/network/api";
 import { message } from "@/assets/js/message";
 import BaseHeader from "@/components/BaseHeader.vue";
-import {mapState} from "vuex"
+import { mapState } from "vuex";
 // TODO 测试正确性
 export default {
-  name:"task",
-  components:{
-    BaseHeader
+  name: "task",
+  components: {
+    BaseHeader,
   },
   data() {
     return {
@@ -111,10 +130,10 @@ export default {
         name: "",
         project: null,
         projectName: "",
-        cataloger:null,
-        catalogerName:"",
-        auditor:null,
-        auditorName:"",
+        cataloger: null,
+        catalogerName: "",
+        auditor: null,
+        auditorName: "",
         createTime: "",
         status: "",
       },
@@ -129,86 +148,102 @@ export default {
             trigger: "blur",
           },
         ],
-        catalogerName:[
-          { required: true, message: "请指定编目员" },
-        ],
-        auditorName:[
-          { required: true, message: "请指定编目员" },
-        ]
+        catalogerName: [{ required: true, message: "请指定编目员" }],
+        auditorName: [{ required: true, message: "请指定编目员" }],
       },
       // 任务列表数据
-      tableData:[],
+      tableData: [],
     };
   },
   computed: {
     // 创建时间的时间戳转化为时间
-    ...mapState("common",["currentProject","currentTaskPage"]),
+    ...mapState("common", ["currentProject", "currentTaskPage"]),
     createTime() {
       return new Date(this.projectForm.createTime).toLocaleString();
     },
   },
   methods: {
     // back返回项目管理页面
-    back(){
+    back() {
       this.$router.push("/manage/project");
     },
     // @see: https://element.eleme.cn/#/zh-CN/component/input#yuan-cheng-sou-suo
-    queryCatalogers(queryString, cb){
-      try{
-        let res = this.$api.queryCatalogerByName(queryString);
-        console.log(res)
-      }catch(e){
-        console.log(e)
+    async queryCatalogers(queryString, cb) {
+      try {
+        if (queryString) {
+          let res = await $api.queryCatalogerByName(queryString);
+          console.log(res);
+          if (res.code === 200 && res.data.length !== 0) {
+            res.data.map((record) => {
+              record.value = record.username + "(" + record.account + ")";
+            });
+            cb(res.data);
+          }
+        }
+      } catch (e) {
+        console.log(e);
       }
       // 从服务器查询编目员
     },
-    queryAuditors(queryString, cb){
-      try{
-        let res = this.$api.queryAuditorByName(queryString);
-        console.log(res)
-      }catch(e){
-        console.log(e)
+    async queryAuditors(queryString, cb) {
+      try {
+        if (queryString) {
+          let res = await $api.queryAuditorByName(queryString);
+          console.log(res);
+          if (res.code === 200 && res.data.length !== 0) {
+            res.data.map((record) => {
+              record.value = record.username + "(" + record.account + ")";
+            });
+            cb(res.data);
+          }
+        }
+      } catch (e) {
+        console.log(e);
       }
       // 从服务器查询审核员
     },
-    handleSelectCataloger(item){
+    handleSelectCataloger(item) {
       // 选择编目员操作
-      console.log(item);
+      console.log("选择编目员",item);
     },
-    handleSelectAuditor(item){
+    handleSelectAuditor(item) {
       // 选择审核员操作
       console.log(item);
     },
     // 更新操作
-    handleEdit(row){
-      if(row.isEdit){
-        this.updateTaskInfo(row.id, row)
+    handleEdit(row) {
+      if (row.isEdit) {
+        this.updateTaskInfo(row.id, row);
       }
       row.isEdit = !row.isEdit;
     },
-    async updateTaskInfo(id, row){
+    async updateTaskInfo(id, row) {
       // 保存数据到后端
-      let res = await $api.updateTaskInfo(id)
-      if(res.code === 200){
+      let res = await $api.updateTaskInfo(id);
+      if (res.code === 200) {
         this.getTaskData();
-      }else{
-        message.error(res.message)
+      } else {
+        message.error(res.message);
       }
     },
     // 删除操作
-    async handleDelete(index, row){
-      let res = await $api.deleteTask(row.id)
-      if(res.code === 200){
+    async handleDelete(index, row) {
+      let res = await $api.deleteTask(row.id);
+      if (res.code === 200) {
         this.getTaskData();
-      }else{
-        message.error(res.message)
+      } else {
+        message.error(res.message);
       }
     },
     // 进入编目操作
-    async enterTask(row){
-      console.log("进入编目")
-      this.$store.commit("storedTaskInfo", {currentPage: this.currentPage, id: row.id, name: row.name})
-        // this.$router.push("/catalog");
+    async enterTask(row) {
+      console.log("进入编目");
+      this.$store.commit("storedTaskInfo", {
+        currentPage: this.currentPage,
+        id: row.id,
+        name: row.name,
+      });
+      // this.$router.push("/catalog");
     },
     // 打开添加项目浮窗，初始化数据
     // TODO 添加权限验证
@@ -233,14 +268,14 @@ export default {
             let res = await $api.addTask(this.taskForm);
             if (res.code === 200) {
               this.dialogVisible = false;
-              this.taskForm= {
+              this.taskForm = {
                 name: "",
                 project: null,
                 projectName: "",
-                cataloger:null,
-                catalogerName:"",
-                auditor:null,
-                auditorName:"",
+                cataloger: null,
+                catalogerName: "",
+                auditor: null,
+                auditorName: "",
                 createTime: "",
                 status: "",
               };
@@ -277,25 +312,35 @@ export default {
         );
         if (res.code === 200) {
           // 展示之前做处理，添加index和status
-          if(res.data === null) return;
+          if (res.data === null) return;
           let start = (this.currentPage - 1) * pageSize + 1;
           res.data.records.map((record) => {
             // 插入index
             record.index = start++;
             // 插入status
-            switch(record.status){
-              case 0: record.status = "创建"; break;
-              case 1: record.status = "编目中"; break;
-              case 2: record.status = "审核中"; break;
-              case 3: record.status = "完成"; break;
-              default: record.status="error"; break;
+            switch (record.status) {
+              case 0:
+                record.status = "创建";
+                break;
+              case 1:
+                record.status = "编目中";
+                break;
+              case 2:
+                record.status = "审核中";
+                break;
+              case 3:
+                record.status = "完成";
+                break;
+              default:
+                record.status = "error";
+                break;
             }
             record.isEdit = false;
             // 转换时间戳
             record.createTime = new Date(record.createTime).toLocaleString();
             return record;
           });
-          this.tableData = res.data.records ?res.data.records : [];
+          this.tableData = res.data.records ? res.data.records : [];
           this.total = res.data.total;
         } else {
           message.error(res.message);
@@ -310,7 +355,7 @@ export default {
     this.currentPage = this.currentTaskPage;
     this.getTaskData();
   },
-}
+};
 </script>
 
 <style>
